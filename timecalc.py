cd0606951b1f2c4e878f9cd294909abc8940c921
Mike Hauss <mhauss@localhost.localdomain>
2017-03-08 13:33:13 -0600

    Additional refactoring changes

--- timecalc.py
+++ timecalc.py
@@ -3,19 +3,20 @@
 '''
 Written by: Mike Hauss
 
-This script accepts a whole number as a number of seconds and tells how many years, months, days, weeks, days, hours, minutes and seconds it is equal to.
+This script accepts a whole number as a number of seconds and tells how many
+years, months, days, weeks, days, hours, minutes and seconds it is equal to.
 If the argument is -m; -u; -d; -w and/or -y, tell how many seconds
 '''
 import pdb
 import argparse
 
 # Variables
-yearSecs = 31536000
-monthSecs = 2592000  # 30 day month
-weekSecs = 604800
-daySecs = 86400
-hourSecs = 3600
-minSecs = 60
+year_secs = 31536000
+month_secs = 2592000  # 30 day month
+week_secs = 604800
+day_secs = 86400
+hour_secs = 3600
+min_secs = 60
 years = ''
 months = ''
 weeks = ''
@@ -23,87 +24,95 @@ days = ''
 hours = ''
 mins = ''
 seconds = ''
-secsRemain = ''
+secs_remain = ''
 
-def main():
+def main():   # parses input
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s','--secs', action="store", help="Seconds; must be a positive whole number", type=int)
-    parser.add_argument('-m','--mins', action="store", help="Minutes; must be a positive whole number", type=int)
-    parser.add_argument('-u','--hours', action="store", help="hoUrs; must be a positive whole number", type=int)
-    parser.add_argument('-d','--days', action="store", help="Days; must be a positive whole number", type=int)
-    parser.add_argument('-w','--weeks', action="store", help="Weeks; must be a positive whole number", type=int)
-    parser.add_argument('-o','--months', action="store", help="mOnths; must be a positive whole number", type=int)
-    parser.add_argument('-y','--years', action="store", help="Year; must be a positive whole number", type=int)
+    parser.add_argument('-s','--secs', action="store",
+            help="Seconds; must be a positive whole number", type=int)
+    parser.add_argument('-m','--mins', action="store",
+            help="Minutes; must be a positive whole number", type=int)
+    parser.add_argument('-u','--hours', action="store",
+            help="hoUrs; must be a positive whole number", type=int)
+    parser.add_argument('-d','--days', action="store",
+            help="Days; must be a positive whole number", type=int)
+    parser.add_argument('-w','--weeks', action="store",
+            help="Weeks; must be a positive whole number", type=int)
+    parser.add_argument('-o','--months', action="store",
+            help="mOnths; must be a positive whole number", type=int)
+    parser.add_argument('-y','--years', action="store",
+            help="Year; must be a positive whole number", type=int)
     args = parser.parse_args()
+    compare(args.secs)
 
-def compare(u): 
+def compare(u):
     #pdb.set_trace()
-    if u > yearSecs:
-        calcYears(u)    # each of these is a list with remainder as [1]
-    elif u > monthSecs:
-        calcMonths(u)
-    elif u > weekSecs:
-        calcWeeks(u)
-    elif u > daySecs:
-        calcDays(u)
-    elif u > hourSecs:
-        calcHours(u)
-    elif u >= minSecs:
-        calcMins(u)
+    if u > year_secs:
+        global years
+        years = calc_years(u)[0]
+        secs_remain = calc_years(u)[1]
+    elif u > month_secs:
+        global months
+        months = calc_months(u)[0]
+        secs_remain = calc_months(u)[1]
+    elif u > week_secs:
+        global weeks
+        weeks = calc_weeks(u)[0]
+        secs_remain = calc_weeks(u)[1]
+    elif u > day_secs:
+        global days
+        days = calc_days(u)[0]
+        secs_remain = calc_days(u)[1]
+    elif u > hour_secs:
+        global hours
+        hours = calc_hours(u)[0]
+        secs_remain = calc_hours(u)[1]
+        print "Hours: %s" % hours
+    elif u >= min_secs:
+        global mins
+        mins = calc_mins(u)
+        secs_remain = calc_mins(u)[1]
     else:
         seconds = u
 
-def calcYears(s):
-    values = divmod(s,yearSecs)     # returns a list
-    numYears = values[0]            # whole number
-    secsRemain = values[1]          # remainder
+# each of these is a list with remainder as [1]
+def calc_years(s):
+    values = divmod(s,year_secs)     # returns a list
+    num_years = values[0]            # whole number
+    remainder = values[1]          # remainder
     # leap year, add a day
-    if numYears > 3:
-        secsRemain += 86400
-    return [numYears, secsRemain]
+    if num_years > 3:
+        remainder += 86400
+    print "Years"
+    return [num_years, remainder]
+
+def calc_months(s):
+    values = divmod(s,month_secs)
+    print "Months"
+    return [values[0], values[1]]
 
 #pdb.set_trace()
-def calcMonths(s):
-    values = divmod(s,monthSecs)
-    numMonths = values[0]
-    secsRemain = values[1]
-    global months
-    return [numMonths, secsRemain]
+def calc_weeks(s):
+    values = divmod(s,week_secs)
+    print "Weeks"
+    return [values[0], values[1]]
 
 #pdb.set_trace()
-def calcWeeks(s):
-    values = divmod(s,weekSecs)
-    numWeeks = values[0]
-    secsRemain = values[1]
-    global weeks
-    return [numWeeks, secsRemain]
+def calc_days(s):
+    values = divmod(s,day_secs)
+    print "Days"
+    return [values[0], values[1]]
 
 #pdb.set_trace()
-def calcDays(s):
-    values = divmod(s,daySecs)
-    numDays = values[0]
-    secsRemain = values[1]
-    global days
-    return [numDays, secsRemain]
+def calc_hours(s):
+    values = divmod(s,hour_secs)
+    return [values[0], values[1]]
 
 #pdb.set_trace()
-def calcHours(s):
-    values = divmod(s,hourSecs)
-    numHours = values[0]
-    secsRemain = values[1]
-    global hours
-    return [numHours, secsRemain]
-
-#pdb.set_trace()
-def calcMins(s):
-    values = divmod(s,minSecs)
-    numMins = values[0]
-    secsRemain = values[1]
-    global mins
-    if secsRemain > 0:
-        return [numMins, secsRemain]
-    else:
-        return numMins
+def calc_mins(s):
+    values = divmod(s,min_secs)
+    print "Minutes"
+    return [values[0], values[1]]
 
 if __name__ == '__main__':
     main()
