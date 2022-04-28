3d149bb31e56b3de44f6ad317e4cb03c00569c18
Mike Hauss <mikehauss@gmail.com>
2022-04-28 15:06:32 -0500

    Commiting pending changes

--- timecalc.py
+++ timecalc.py
@@ -39,28 +39,21 @@ def main():   # parses input
     compare(s,m,u,d,w,o,y)
     # Print results at end
 
-def compare(s,m,u,d,w,o,y):
+#def compare(s,m,u,d,w,o,y):
     # Syntax: datetime.timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
-    if s is None:
-        s = 0
-    if m is None:
-        m = 0
-    if u is None:
-        u = 0
-    if d is None:
-        d = 0
-    if w is None:
-        w = 0
-    if o is None:
-        o = 0
-    if y is None:
-        y = 0
-    #print(s,m,u,d,w,o,y)
-    #print("{country}: {capital}".format(country=c, capital=capital_country[c]))
+#    none_to_zero(s,m,u,d,w,o,y)
+def compare(s,m,u,d,w,o,y):
+    if s is None: s = 0
+    if m is None: m = 0
+    if u is None: u = 0
+    if d is None: d = 0
+    if w is None: w = 0
+    if o is None: o = 0
+    if y is None: y = 0
     print("Seconds: {a}, Minutes: {b}, Hours: {c}, Days: {x}, Weeks: {e}, Months: {f} Years: {g}".format(a=s,b=m,c=u,x=d,e=w,f=o,g=y))
 # compare inputted time to each unit
 '''
-def compare(u):
+def compare(none_to_zero):
     y = timedelta(days=365)
     year_secs = y.total_seconds()
     m = timedelta(days=30)
@@ -73,7 +66,6 @@ def compare(u):
     hour_secs = h.total_seconds()
     i = timedelta(mins=1)
     min_secs = i.total_seconds()
-    
    
     if u >= year_secs:
         values = divmod(u,year_secs)
